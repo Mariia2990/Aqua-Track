f44251411239d13db3b300916d0d5065854b28a8
Mariia <masyakuk@gmail.com>
2025-03-05 19:05:34 +0200

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@
 
 // const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 // const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
-// const LoginPage = lazy(() => import("./pages/LogInPage/LogInPage"));
+// const SignInPage = lazy(() => import("./pages/SignInPage/SignInPage"));
 // const TrackerPage = lazy(() => import("./pages/TrackerPage/TrackerPage"));
 // const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
@@ -14,18 +14,17 @@ export const App = () => {
 
   return (
     <>
-          {/* <Routes>
+      {/* <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/signup" element={
                 <RestrictedRoute redirectTo="/tracker" component={<SignUpPage />}/>}/>
             <Route path="/signin" element={
-                <RestrictedRoute redirectTo="/tracker" component={<LoginPage />}/>}/>
+                <RestrictedRoute redirectTo="/tracker" component={<SignInPage />}/>}/>
             <Route path="/tracker" element={
                 <PrivateRoute redirectTo="/signin" component={<TrackerPage />}/>}/>
 
             <Route path="*" element={<NotFoundPage />} />
           </Routes> */}
-
     </>
-  )
+  );
 };
