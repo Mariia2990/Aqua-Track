7df6a5903cc875ad6bebc589bc2c2be492304dd0
Maksym <mvsozykin@gmail.com>
2025-03-06 21:28:27 +0200

    UserPanel updated

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,14 +1,30 @@
-// import css from "./UserPanel.module.css";
-
-import { UserBar } from "../UseBar/UserBar";
-
+// import { useDispatch, useSelector } from 'react-redux';
+import { UserBar } from '../UseBar/UserBar';
+import css from './UserPanel.module.css';
+// import { useEffect } from 'react';
 
 export function UserPanel() {
- 
+  // const { user } = useAuth();
+  // const dispatch = useDispatch();
+  // const userData = useSelector(selectUser);
+
+  // useEffect(() => {
+  //   if (!user) {
+  //     dispatch(refreshUser());
+  //   }
+  // }, [dispatch, user]);
+
+  // const getFirstName = (fullName) => {
+  //   return fullName ? fullName.split(' ')[0] : 'User';
+  // };
 
   return (
-    <>
+    <div className={css.userPanel}>
+      <h2 className={css.welcomeTitle}>
+        Hello
+        <span className={css.name}>, !</span>
+      </h2>
       <UserBar />
-    </>
+    </div>
   );
 }
