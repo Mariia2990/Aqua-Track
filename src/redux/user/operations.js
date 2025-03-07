b62854cc922bb323c1440c0f53e49fb851102a1e
Vasyl Nikolov <vnnikolov1991@gmail.com>
2025-03-07 13:47:29 +0100

    fix3

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -1,56 +1,14 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-import { setUserInfo, setAvatar, setLoading, setError } from "./slice";
-
-// Базовый URL сервера 
-axios.defaults.baseURL = "https://your-api.com/api";
-
-export const fetchUser = createAsyncThunk("user/fetchUser", async (_, thunkAPI) => {
-  try {
-    thunkAPI.dispatch(setLoading(true)); 
-    const response = await axios.get("/user");
-    return response.data;
-  } catch (error) {
-    thunkAPI.dispatch(setError(error.message)); 
-    return thunkAPI.rejectWithValue(error.message);
-  } finally {
-    thunkAPI.dispatch(setLoading(false)); 
-  }
-});
-
-
-export const updateUser = createAsyncThunk("user/updateUser", async (userData, thunkAPI) => {
-  try {
-    thunkAPI.dispatch(setLoading(true));
-    const response = await axios.patch("/user", userData);
-    thunkAPI.dispatch(setUserInfo(response.data)); 
-    return response.data;
-  } catch (error) {
-    thunkAPI.dispatch(setError(error.message));
-    return thunkAPI.rejectWithValue(error.message);
-  } finally {
-    thunkAPI.dispatch(setLoading(false));
-  }
-});
-
-
-export const updateAvatar = createAsyncThunk("user/updateAvatar", async (avatarFile, thunkAPI) => {
-  try {
-    thunkAPI.dispatch(setLoading(true));
-
-    const formData = new FormData();
-    formData.append("avatar", avatarFile);
-
-    const response = await axios.patch("/user/avatar", formData, {
-      headers: { "Content-Type": "multipart/form-data" },
-    });
-
-    thunkAPI.dispatch(setAvatar(response.data.avatarURL)); 
-    return response.data.avatarURL;
-  } catch (error) {
-    thunkAPI.dispatch(setError(error.message));
-    return thunkAPI.rejectWithValue(error.message);
-  } finally {
-    thunkAPI.dispatch(setLoading(false));
-  }
-});
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+axios.defaults.baseURL = 'урл апі бекенду';
+export const getUsers = createAsyncThunk(
+    'users/get',
+    async (_, thunkAPI) => {
+        try {
+            const res = await axios.get('роут з бекенду');
+            return res.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
+    }
+)
--- src/redux/user/selectors.js
+++ src/redux/user/selectors.js
@@ -1,15 +1,3 @@
-export const selectUserInfo = (state) => state.user.userInfo;
-
-export const selectDailyNorm = (state) => state.user.userInfo.dailyNorm;
-
-export const selectAvatarURL = (state) => state.user.avatarURL;
-
-export const selectMonthData = (state) => state.user.monthData;
-
-export const selectTodayWaterList = (state) => state.user.todayWaterList;
-
-export const selectWaterList = (state) => state.user.waterList; 
-
-
-export const selectUserErrorMessage = (state) => state.user.isErrorMessage;
-export const selectUserIsLoading = (state) => state.user.isLoading;
+export const selectUsersCount = (state) => state.users.count;
+export const selectUsersAvatars = (state) => state.users.avatars;
+export const selectLoading = (state) => state.users.loading;
