b86e45590016823d1e64c753302b0ac6d63218a2
Mariia <masyakuk@gmail.com>
2025-03-08 14:08:28 +0200

    add PR, RR

--- src/RestrictedRoute.jsx
+++ src/RestrictedRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useAuth } from './hook/useAuth';
 
-export const RestrictedRoute = ({ element, redirectTo }) => {
-  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+export const RestrictedRoute = ({ element, redirectTo = '/' }) => {
+  const { isLoggedIn, isRefreshing } = useAuth();
+
+  if (isRefreshing && localStorage.getItem('refreshToken')) {
+    return null;
+  }
   return isLoggedIn ? <Navigate to={redirectTo} /> : element;
 };
