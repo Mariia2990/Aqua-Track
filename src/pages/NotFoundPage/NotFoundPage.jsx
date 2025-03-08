c4e0dec47e784d69e3cc2edb759f262b0fe454f5
Mariia <masyakuk@gmail.com>
2025-03-08 21:33:44 +0200

    add Not Found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,23 @@
-// import css from "./NotFoundPage.module.css";
+import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
+import { BiGhost } from 'react-icons/bi';
+import { Logo } from "../../components/Logo/Logo";
+import Container  from "../../components/Container/Container";
+
 
 export default function NotFoundPage() {
   return (
-    <>
-    </>
+    <Container>
+      <Logo />
+      <div className={css.NFBox2}>
+        <BiGhost size={100} color="#9be1a0" />
+        <h2 className={css.NFTitle1}>Page not found - 404</h2>
+        <div className={css.NFButtonBox}>
+          <Link to="/" className={css.NFButton_tracker}>
+            Go home
+          </Link>
+        </div>
+      </div>
+    </Container>
   );
 }
