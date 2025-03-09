c3a25b87fa35d59f2f72fe9b5918decc62dbc5e0
Mariia <masyakuk@gmail.com>
2025-03-09 12:13:59 +0200

    update main

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Suspense } from "react";
+import { Loader } from "../Loader/Loader";
 
 export const Layout = () => {
   return (
     <>
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader absolute={true}/>}>
           <Outlet />
         </Suspense>
     </>
