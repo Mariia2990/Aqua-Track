8976530c6e3d2609a2d905bce11299d299c9dcb0
Maksym <mvsozykin@gmail.com>
2025-03-09 18:07:49 +0200

    usersettingsmodal style added

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,5 +1,5 @@
 // import { useDispatch, useSelector } from 'react-redux';
-// import { useEffect } from 'react';
+// import { useEffect, useState } from 'react';
 // import { useAuth } from '../../hook/useAuth.jsx';
 // import { selectUser } from '../../redux/auth/selectors.js';
 // import { refreshUser } from '../../redux/auth/operations.js';
@@ -10,12 +10,20 @@ export function UserPanel() {
   // const { user } = useAuth();
   // const dispatch = useDispatch();
   // const userData = useSelector(selectUser);
+  // const [isUserUpdated, setIsUserUpdated] = useState(false);
 
   // useEffect(() => {
   //   if (!user) {
   //     dispatch(refreshUser());
   //   }
   // }, [dispatch, user]);
+
+  // useEffect(() => {
+  //   if (isUserUpdated) {
+  //     dispatch(refreshUser());
+  //     setIsUserUpdated(false);
+  //   }
+  // }, [dispatch, isUserUpdated]);
 
   // const getFirstName = (fullName) => {
   //   return fullName ? fullName.split(' ')[0] : user.email.split('@')[0];
@@ -38,4 +46,4 @@ export function UserPanel() {
   );
 }
 
-// user={userData} getFirstName={getFirstName}
+// user={userData} getFirstName={getFirstName} setIsUserUpdated={setIsUserUpdated}
