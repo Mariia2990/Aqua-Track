f8f4e76f2ee1ff53d272517b4e091a135fd7122c
Oksana Cherviachenko <kvintinovna@gmail.com>
2025-03-09 22:57:20 +0100

    update SignUpPage.jsx

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -2,15 +2,15 @@
 import { SignUpForm } from "../../components/SignUpForm/SignUpForm";
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import Container from "../../components/Container/Container";
-//import Logo from "../../components/Logo/Logo";  // ❗️ Закоментований код залишено без змін
+import Logo from "../../components/Logo/Logo";  
 import styles from "./SignUpPage.module.css";
-import containerStyles from "../../components/Container/Container.module.css"; // Імпорт стилів контейнера
+import containerStyles from "../../components/Container/Container.module.css"; 
 
 export default function SignUpPage() {
   return (
     <Container>
-      <div className={containerStyles.container}> {/* Використання правильного класу */}
-        {/*<Logo />*/}  {/* ❗️ Закоментований код залишено без змін */}
+      <div className={containerStyles.container}> 
+        <Logo />  
         <SignUpForm />
         <AdvantagesSection />
       </div>
