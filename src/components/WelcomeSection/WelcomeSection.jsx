f1b5c6134f20dfa24829899a8ef1a1ade4887a68
Maksym Gulko <maksymgulko@gmail.com>
2025-03-09 13:44:19 +0100

    Homepage (#15)
    * add welcome and advantages section
    
    * finish home page, adjusted default export of logo component

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -1,8 +1,23 @@
-import { Logo } from "../Logo/Logo";
-// import css from "./WelcomeSection.module.css";
+import { Logo } from '../Logo/Logo';
+import css from './WelcomeSection.module.css';
+import clsx from 'clsx';
 
 export const WelcomeSection = () => {
   return (
+    <div className={css.container}>
       <Logo />
+      <div className={css.wrapper}>
+        <div className={css.text}>
+          <p className={css.hintText}>Record daily water intake and track</p>
+          <p className={css.mainText}>Water consumption tracker</p>
+        </div>
+        <div className={css.buttons}>
+          <button className={clsx('buttonGreen', css.btngreen)}>
+            Try tracker
+          </button>
+          <button className={clsx('cancelButton', css.btngray)}>Sign In</button>
+        </div>
+      </div>
+    </div>
   );
 };
