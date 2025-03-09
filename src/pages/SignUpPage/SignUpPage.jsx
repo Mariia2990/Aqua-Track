45bfbcfc9cf3e226206e94bb069c41cff66f6cee
Oksana Cherviachenko <kvintinovna@gmail.com>
2025-03-09 22:38:28 +0100

    Resolve conflicts after stash pop

--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,22 +1,22 @@
 //src/pages/SignUpPage/SignUpPage.jsx
-import { Suspense } from "react";
 import { SignUpForm } from "../../components/SignUpForm/SignUpForm";
 import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
 import Container from "../../components/Container/Container";
-import Logo from "../../components/Logo/Logo";
+//import Logo from "../../components/Logo/Logo";  // ❗️ Закоментований код залишено без змін
 import styles from "./SignUpPage.module.css";
+import containerStyles from "../../components/Container/Container.module.css"; // Імпорт стилів контейнера
 
 export default function SignUpPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}> 
-      <Container>
-        <Logo />
-        <h2 className={styles.title}>Create an account</h2>
+    <Container>
+      <div className={containerStyles.container}> {/* Використання правильного класу */}
+        {/*<Logo />*/}  {/* ❗️ Закоментований код залишено без змін */}
         <SignUpForm />
         <AdvantagesSection />
-      </Container>
-    </Suspense>
+      </div>
+    </Container>
   );
 }
+
 
 
