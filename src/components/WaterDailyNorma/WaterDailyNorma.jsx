7642581f60df070890e13353fdb2776e579ae49f
Rinvernal <167707300+Rinvernal@users.noreply.github.com>
2025-03-10 09:52:01 +0200

    WaterMainInfo (#20)
    * visually ready
    
    * fix media
    
    ---------
    
    Co-authored-by: Марія Лозинська <masyakuk@gmail.com>

--- src/components/WaterDailyNorma/WaterDailyNorma.jsx
+++ src/components/WaterDailyNorma/WaterDailyNorma.jsx
@@ -1,5 +1,16 @@
-// import css from "./WaterDailyNorma.module.css";
+import css from "./WaterDailyNorma.module.css";
 
-export const WaterDailyNorma = () => {
-  return <></>;
+export const WaterDailyNorma = ({className, dailyNorm, setDailyNorm}) => {
+  const changeNorm = () => {
+    const newNorm = parseFloat(prompt("Enter your daily water intake (liters):") || "1.5");
+    if (!isNaN(newNorm) && newNorm > 0) {
+      setDailyNorm(newNorm);
+    }
+  };
+
+  return (
+    <div className={`${css.wrapper} ${className}`} onClick={changeNorm}>
+      <p className={css.day}>{dailyNorm} L</p>
+      <p className={css.norma1}>My daily norma</p>
+    </div>);
 };
