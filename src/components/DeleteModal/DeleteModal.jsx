3acf1d90b05ce8d4c71a82cb9c9a2b94b3648d42
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-03-11 13:02:58 +0200

    add markup and styles to deleteModal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,12 +1,22 @@
-// import css from "./DeleteModal.module.css";
-import { ModalButton } from "../ModalButton/ModalButton";
+import { GlobalModal } from '../GlobalModal/GlobalModal';
 
-export const DeleteModal = () => {
+import css from './DeleteModal.module.css';
 
+export const DeleteModal = ({ id, isOpen, onClose }) => {
   return (
-    <>
-      <ModalButton text={"Delete"}></ModalButton>
-      <ModalButton text={"Cancel"}></ModalButton>
-    </>
+    <GlobalModal isOpen={isOpen} onClose={onClose}>
+      <div className={css.modal}>
+        <h2 className={css.modal__title}>Delete entry</h2>
+        <p className={css.modal__subtitle}>
+          Are you sure you want to delete this entry?
+        </p>
+        <div className={css.modal__buttons}>
+          <button className={`buttonGreen ${css.modal__button}`}>Delete</button>
+          <button className={`cancelButton ${css.modal__button}`} onClick={onClose}>
+            Cancel
+          </button>
+        </div>
+      </div>
+    </GlobalModal>
   );
 };
