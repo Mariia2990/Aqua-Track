1e5c5e351263a4971c748b025e26ab9c31ee0a94
Марія Лозинська <masyakuk@gmail.com>
2025-03-13 14:56:09 +0200

    Update refresh (#33)
    * add refresh token
    
    * update refresh

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,90 +1,69 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { login, logOut, refreshUser, register, updateUser } from './operations';
 
+const initialState = {
+  user: {
+    email: null,
+    name: null,
+    gender: null,
+    avatar: null,
+    weight: null,
+    DailyActivityTime: null,
+    DailyWaterNorm: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  error: null,
+};
+
 const slice = createSlice({
   name: 'auth',
-  initialState: {
-    user: {
-      email: null,
-      name: null,
-      gender: null,
-      avatar: null,
-      weight: null,
-      DailyActivityTime: null,
-      DailyWaterNorm: null,
-    },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
-    error: null, 
-  },
+  initialState,
   reducers: {
     setToken(state, action) {
       state.token = action.payload.token;
-      state.refreshToken = action.payload.refreshToken;
       state.isLoggedIn = true;
     },
   },
   extraReducers: (builder) => {
     builder
-      .addCase(register.pending, (state) => {
-        state.error = null; 
-      })
       .addCase(register.fulfilled, (state, action) => {
         state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.refreshToken = action.payload.refreshToken;
-        state.isLoggedIn = true; 
-      })
-      .addCase(register.rejected, (state, action) => {
-        state.error = action.payload;
+        state.token = action.payload.accessToken;
+        localStorage.setItem('refreshToken', action.payload.refreshToken);
+        state.isLoggedIn = true;
       })
       .addCase(login.fulfilled, (state, action) => {
         state.user = action.payload.user;
-        state.token = action.payload.token;
-        state.refreshToken = action.payload.refreshToken;
+        state.token = action.payload.accessToken;
+        localStorage.setItem('refreshToken', action.payload.refreshToken);
         state.isLoggedIn = true;
-      })
-      .addCase(login.rejected, (state, action) => {
-        state.error = action.payload;
+        // setAuthHeader(action.payload.accessToken);
       })
       .addCase(logOut.fulfilled, (state) => {
-        state.user = {
-          email: null,
-          name: null,
-          gender: null,
-          avatar: null,
-          weight: null,
-          DailyActivityTime: null,
-          DailyWaterNorm: null,
-        };
+        state.user = initialState.user;
         state.token = null;
+        localStorage.removeItem('refreshToken');
         state.isLoggedIn = false;
       })
       .addCase(refreshUser.pending, (state) => {
         state.isRefreshing = true;
       })
       .addCase(refreshUser.fulfilled, (state, action) => {
-        state.user = action.payload;
+        state.user = action.payload.user;
+        state.token = action.payload.accessToken;
+        localStorage.setItem('refreshToken', action.payload.refreshToken);
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
       .addCase(refreshUser.rejected, (state) => {
         state.isRefreshing = false;
-      })
-      .addCase(updateUser.pending, (state) => {
-        state.isRefreshing = true;
+        state.token = null;
+        localStorage.removeItem('refreshToken');
       })
       .addCase(updateUser.fulfilled, (state, action) => {
-        state.error = '';
-        state.isRefreshing = false;
-        state.user.email = action.payload.email;
-        state.user.name = action.payload.name;
-        state.user.gender = action.payload.gender;
-        state.user.avatar = action.payload.avatarUrl;
-        state.user.weight = action.payload.weight;
-        state.user.DailyActivityTime = action.payload.DailyActivityTime;
-        state.user.DailyWaterNorm = action.payload.DailyWaterNorm;
+        state.user = { ...state.user, ...action.payload };
       })
       .addCase(updateUser.rejected, (state, action) => {
         state.error = action.payload;
@@ -93,4 +72,3 @@ const slice = createSlice({
 });
 
 export const authReducer = slice.reducer;
-
