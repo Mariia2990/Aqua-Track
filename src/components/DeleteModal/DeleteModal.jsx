092beef4d5dd39fbc13d28ce58e0553b9f02b17b
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-03-14 14:04:52 +0000

    add deleteModal

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -1,8 +1,23 @@
+import { useDispatch } from 'react-redux';
+
 import { GlobalModal } from '../GlobalModal/GlobalModal';
+import { deleteWater } from '../../redux/water/operations';
 
 import css from './DeleteModal.module.css';
 
 export const DeleteModal = ({ id, isOpen, onClose }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = async () => {
+    try {
+      await dispatch(deleteWater({ id }));
+      toast.success('Water entry deleted successfully!');
+      onClose();
+    } catch (error) {
+      toast.error('Failed to delete water entry.');
+    }
+  };
+
   return (
     <GlobalModal isOpen={isOpen} onClose={onClose}>
       <div className={css.modal}>
@@ -11,8 +26,16 @@ export const DeleteModal = ({ id, isOpen, onClose }) => {
           Are you sure you want to delete this entry?
         </p>
         <div className={css.modal__buttons}>
-          <button className={`buttonGreen ${css.modal__button}`}>Delete</button>
-          <button className={`cancelButton ${css.modal__button}`} onClick={onClose}>
+          <button
+            className={`buttonGreen ${css.modal__button}`}
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+          <button
+            className={`cancelButton ${css.modal__button}`}
+            onClick={onClose}
+          >
             Cancel
           </button>
         </div>
