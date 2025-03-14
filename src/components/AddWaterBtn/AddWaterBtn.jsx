1b103f6a6426bd97eef76f45a01334e154048be4
Rinvernal <167707300+Rinvernal@users.noreply.github.com>
2025-03-14 07:37:14 +0200

    fix bug3 (#34)
    Co-authored-by: Марія Лозинська <masyakuk@gmail.com>

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,8 +1,10 @@
 import css from "./AddWaterBtn.module.css";
 import { BaseModal } from "../BaseModal/BaseModal";
 import { WaterModal } from '../WaterModal/WaterModal';
+import { useState } from "react";
 
 export function AddWaterBtn({className, onAddWater }) {
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
     const amount = parseFloat(prompt("Enter amount of water (liters):") || "0");
@@ -13,13 +15,18 @@ export function AddWaterBtn({className, onAddWater }) {
 
   return (
     <div className={`${css.wrapper} ${className}`}>
-    <button className={css.addButton} onClick={handleClick}>
-      
-      Add water
-    </button>
-      {/* <BaseModal>
-        <WaterModal />
-      </BaseModal> */}
+      <button className={css.addButton} onClick={handleClick}>
+        <svg width="16px" height="16px" fill="#fff">
+          <use href="/public/img/sprite.svg#icon-plus-stroke"/>
+        </svg>
+        Add water
+      </button>
+
+      {isOpen && (
+        <BaseModal>
+          <WaterModal onAddWater={onAddWater} />
+        </BaseModal>
+      )}
     </div>
   );
 }
