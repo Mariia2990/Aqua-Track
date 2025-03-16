b9d0105dd38bf1bc5ed993bf4603f2c9ea804554
Serhii Sokolov <68744408+Falconoff@users.noreply.github.com>
2025-03-16 09:52:25 +0200

    add: count & users avatars render (#42)

--- src/redux/user/operations.js
+++ src/redux/user/operations.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 axios.defaults.baseURL = 'https://aquatrackerapi.onrender.com';
+// axios.defaults.baseURL = 'http://localhost:3000';
+
 export const getUsers = createAsyncThunk('users/get', async (_, thunkAPI) => {
   try {
-    const res = await axios.get('/users');
-    return res.data.data;
+    const res = await axios.get('/users/counter');
+    return res.data;
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
   }
