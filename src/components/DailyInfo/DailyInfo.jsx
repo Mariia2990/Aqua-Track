aba79ff7cbe86a4aa0cdb7bfe7274ff25dce8c18
Mariia <masyakuk@gmail.com>
2025-03-17 19:02:53 +0200

    update css

--- src/components/DailyInfo/DailyInfo.jsx
+++ src/components/DailyInfo/DailyInfo.jsx
@@ -1,4 +1,4 @@
-// import css from "./DailyInfo.module.css";
+import css from "./DailyInfo.module.css";
 
 import { AddWaterBtn } from "../AddWaterBtn/AddWaterBtn";
 import { ChooseDate } from "../ChooseDate/ChooseDate";
@@ -6,10 +6,12 @@ import { WaterList } from "../WaterList/WaterList";
 
 export function DailyInfo() {
   return (
-    <>
+    <div>
+      <div className={css.wrapper}>
       <ChooseDate/>
-      <AddWaterBtn section ="daily"/>
+        <AddWaterBtn section="daily" />
+      </div>
       <WaterList />
-    </>
+    </div>
   );
 }
