854b322f0e93ad7d636c5f6a28f5c2e38f09635f
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-03-17 19:13:01 +0000

    disable addWater btn and add border for today in calendar

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -3,14 +3,26 @@ import { WaterModal } from '../WaterModal/WaterModal';
 import { useState } from 'react';
 import clsx from 'clsx';
 import { FaPlus } from 'react-icons/fa6';
+import { useSelector } from 'react-redux';
+import { selectDate } from '../../redux/water/selectors';
 
 export function AddWaterBtn({ className, section }) {
+  const selectedDate = useSelector(selectDate);
   const [isOpen, setIsOpen] = useState(false);
+
+  const today = new Date().toISOString().split('T')[0];
+  const selectedDateFormatted = selectedDate
+    ? selectedDate.split('T')[0]
+    : null;
+
   const classes = (mainClass) =>
     clsx(mainClass, {
       [css.waterMain]: section === 'waterMain',
       [css.daily]: section === 'daily',
+      [css.disabled]: selectedDateFormatted !== today,
     });
+
+  const isDisabled = selectedDateFormatted !== today;
 
   const handleOpenModal = () => {
     setIsOpen(true);
@@ -22,7 +34,11 @@ export function AddWaterBtn({ className, section }) {
 
   return (
     <div className={`${css.wrapper} ${className}`}>
-      <button className={classes(css.wrapperBtn)} onClick={handleOpenModal}>
+      <button
+        className={classes(css.wrapperBtn)}
+        onClick={handleOpenModal}
+        disabled={isDisabled}
+      >
         <FaPlus className={classes(css.plusIcon)} />
         <p className={classes(css.text)}>Add water</p>
       </button>
